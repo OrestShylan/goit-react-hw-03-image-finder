126760e97b52dc8cbc1ed5bfbac28b44e2149d06
OrestShylan <112808022+OrestShylan@users.noreply.github.com>
2023-04-02 13:28:35 +0300

    addet searchbar, api and others

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import * as Api from '../services/services-api';
+import { Searchbar } from './SearchBar/SearchBar';
+
+export class App extends Component {
+  state = {
+    searchValue: '',
+    page: 1,
+    per_page: 12,
+    images: [],
+    showModal: false,
+    largeImageURL: '',
+    isLoader: false,
+    isLoadMore: false,
+  };
+}
